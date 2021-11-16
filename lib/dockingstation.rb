5d154ac6b62c5fe2ada716608203fac9875e9e06
Bryn <bryn2009@live.co.uk>
2021-11-16 16:48:26 +0000

    challenge 10

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,5 +1,9 @@
 class DockingStation
   def release_bike
-    
+    Bike.new
+  end
+
+  def gets_bike
+
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,4 +2,14 @@ require 'dockingstation'
 
 describe DockingStation do
   it { expect(DockingStation.new).to respond_to :release_bike }
+  
+  it 'user gets bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+
+  end
 end
+
+
+
+
