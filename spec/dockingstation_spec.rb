d50ea4df8bb44ad64985d13f9a4be5578710351e
Bryn <bryn2009@live.co.uk>
2021-11-17 15:12:17 +0000

    part 11

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -6,7 +6,15 @@ describe DockingStation do
   it 'user gets bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+  
+  it 'lets the user dock a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
 
+  it 'lets user see docked bike' do
+    expect(subject).to respond_to :bike
   end
 end
 
