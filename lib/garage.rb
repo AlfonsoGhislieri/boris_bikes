d1e15a7e66582306a78c5030b9232114b9668d50
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2021-11-19 18:04:52 +0100

    chp 21

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,3 +1,5 @@
+require_relative 'dockingstation'
+
 class Garage
   attr_reader :storage
   def initialize
@@ -9,5 +11,10 @@ class Garage
       @storage << bike
     end
     van.empty_van
+  end
+
+  def working_to_station(station)
+    @storage.each { | bike| station.bikes << bike if bike.working? }
+    @storage.select! { | bike | !bike.working? }
   end
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -5,13 +5,28 @@ describe Garage do
     @station = DockingStation.new
     @bike = Bike.new
     @van = Van.new
-  end
-
-  it "after delivery, garage storage contains all broken bikes" do
     @bike.report
     @station.dock(@bike)
     @van.send_broken_to_van(@station)
     subject.deliver_to_garage(@van)
+  end
+
+  it "after delivery, garage storage contains all broken bikes" do
     expect(subject.storage.include?(@bike)).to eq true
   end
+
+  it "adds working bike to DockingStation" do
+    bike2 = Bike.new
+    subject.storage.push(bike2)
+    subject.working_to_station(@station)
+    expect(@station.bikes.include?(bike2)).to eq true
+  end
+
+  it "removes working bikes after they have been transported to station" do
+    bike2 = Bike.new
+    subject.storage.push(bike2)
+    subject.working_to_station(@station)
+    expect(subject.storage.include?(bike2)).to eq false
+  end
+
 end
